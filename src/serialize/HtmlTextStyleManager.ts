125987083c36cafc3b96eae58e4fc9396e049070
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2023-04-14 15:02:30 +0200

    mixed bold and italic

--- src/serialize/HtmlTextStyleManager.ts
+++ src/serialize/HtmlTextStyleManager.ts
@@ -1,10 +1,11 @@
-import { TextStyle } from "../styles/TextStyle";
-import { XmlWriter } from "./XmlWriter";
+import { TextStyle, TextStyleProps } from "../styles/TextStyle";
+import { EndScopeFunc, XmlWriter } from "./XmlWriter";
 
 /** @internal */
 export class HtmlTextStyleManager {
     readonly #writer: XmlWriter;
-    #ambient: TextStyle;
+    readonly #ambient: TextStyle;
+    readonly #stack: StackEntry[] = [];
 
     constructor(writer: XmlWriter, ambient: TextStyle) {
         this.#writer = writer;
@@ -12,14 +13,78 @@ export class HtmlTextStyleManager {
     }
 
     public apply(style: TextStyle): void {
-        // TODO
+        // TODO: link
+        // TODO: underline
+        // TODO: strike
+        // TODO: fontFamily
+        // TODO: fontSize
+        // TODO: color
+        this.#applyBooleanStyle("bold", style, "b", { fontWeight: "normal" });
+        this.#applyBooleanStyle("italic", style, "i", { fontStyle: "normal" });
+        // TODO: baseline
     }
 
     public dispose(): void {
-        // TODO
+        this.#stack.splice(0, this.#stack.length).reverse().forEach(({ end }) => end());
+    }
+
+    #applyBooleanStyle(
+        key: keyof TextStyleProps,
+        style: TextStyle,
+        set: string | CssProps,
+        unset: string | CssProps
+    ): void {
+        const value = style[key];
+
+        for (let i = this.#stack.length - 1; i >= 0; --i) {
+            const scoped = this.#stack[i].style[key];
+            if (scoped === value) {
+                return;
+            } else if (scoped !== undefined) {
+                this.#stack.splice(i, this.#stack.length - i).reverse().forEach(({ end }) => end());
+            }
+        }
+
+        if (value !== undefined && this.#ambient[key] !== value) {
+            this.#push(value ? set : unset, { [key]: value });
+        }
+    }
+
+    #push(tagOrCss: string | CssProps, style: Partial<TextStyleProps>): void {
+        let end: EndScopeFunc;
+
+        if (typeof tagOrCss === "string") {
+            end = this.#writer.start(tagOrCss);
+        } else {
+            end = this.#writer.start("span", { style: makeCssString(tagOrCss) });
+        }
+        
+        this.#stack.push({ end, style });
     }
 }
 
+interface StackEntry {
+    end: EndScopeFunc;
+    style: Partial<TextStyleProps>;
+}
 
-const makeCssString = (props: Map<string, string>): string =>
-    [...props].map(([key, value]) => `${key}:${value}`).join(";");
+interface CssProps {
+    fontWeight?: "normal";
+    fontStyle?: "normal";
+}
+
+const CssPropNames: Record<keyof CssProps, string> = {
+    fontWeight: "font-weight",
+    fontStyle: "font-style",
+};
+
+const makeCssString = (props: CssProps): string => {
+    const entries: string[] = [];
+    for (const [key, name] of Object.entries(CssPropNames)) {
+        const value = props[key as keyof CssProps];
+        if (value !== undefined) {
+            entries.push(`${name}:${value}`);
+        }
+    }
+    return entries.join(";");
+};
